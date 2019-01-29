1a83d1b1110bf4db5dd731ce8e5a092ba504fe89
megabass00 <luisma.jig@gmail.com>
2019-01-29 23:08:39 +0100

    Clipboard library added

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -9,14 +9,43 @@ $(document).ready(function(){
 
     // enable tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+    // initialize buttons to copy to clipboard
+    var buttons = document.querySelectorAll('.btn-clipboard');
+    var clipboard = new ClipboardJS('button');
+    clipboard.on('success', (e) => {
+        setTooltip('Copied!');
+        hideTooltip();
+    });
+    clipboard.on('error', (e) => {
+        setTooltip('Failed!');
+        hideTooltip();
+    });
+    console.log(clipboard);
+    
     
     // form focus
-    $('.input-focus').focus(function(){
+    $('.input-focus').focus(() => {
         $(this).css('border-bottom', '2px solid #51ce00');
         console.log($(this));
     });
   
-    $(".input-focus").focusout(function(){
+    $(".input-focus").focusout(() => {
         $(this).css('border-bottom', '1px solid #ced4da');
     });
+
+
+    // FUNCTIONS //
+    function setTooltip(message) {
+        console.log('adkladlaksmdlka');
+        $('.btn-clipboard').tooltip('hide')
+            .attr('data-original-title', message)
+            .tooltip('show');
+    }
+
+    function hideTooltip() {
+        setTimeout(function() {
+            $('.btn-clipboard').tooltip('hide');
+        }, 1000);
+    }
 });
