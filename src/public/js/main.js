0179cec87edb809ab0415e3fd72a112c4962cdad
megabass00 <luisma.jig@gmail.com>
2019-01-30 00:37:57 +0100

    Html fixed

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -22,6 +22,16 @@ $(document).ready(function(){
         hideTooltip();
     });
     console.log(clipboard);
+
+
+    // initialize preview giphy fields
+    $('.preview-giphy input').focusout((e) => {
+        var url = e.target.value;
+        if (validUrl(url)) {
+            console.log($(this));
+        }
+    });
+    
     
     
     // form focus
@@ -47,5 +57,11 @@ $(document).ready(function(){
         setTimeout(function() {
             $('.btn-clipboard').tooltip('hide');
         }, 1000);
+    }
+
+    function validUrl(url) {
+        if (!url) return false;
+        var pattern =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        return pattern.test(url);
     }
 });
