2c1d6de566725dd2af5ccd229826743a8c0feab7
megabass00 <luisma.jig@gmail.com>
2019-01-29 22:33:58 +0100

    Giphy list view added

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+    // initialize datatables
+    $('.datatable-js').DataTable( {
+        "columnDefs": [
+            { "orderable": false, "targets": 3 }
+        ]
+    });
+
+    // enable tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    // form focus
     $('.input-focus').focus(function(){
         $(this).css('border-bottom', '2px solid #51ce00');
         console.log($(this));
--- src/routes/giphies.js
+++ src/routes/giphies.js
@@ -1,21 +1,26 @@
 const router = require('express').Router();
+const Giphy = require('../models/Giphy');
 
-router.get('/giphies', (req, res) => {
-    res.send('List of giphies');
+router.get('/giphies', async (req, res) => {
+    const giphies = await  Giphy.find();
+    res.render('giphies/list', { giphies });
 });
 
 router.get('/giphies/new-giphy', (req, res) => {
     res.render('giphies/add');
 });
 
-router.post('/giphies/add', (req, res) => {
+router.post('/giphies/add', async (req, res) => {
     // console.log(req.body);
     var errors = validateGiphy(req.body);
     const { title, description, url } = req.body;
     if (errors.length > 0) {
         res.render('giphies/add', { errors, title, description, url });
     }else{
-        res.render('giphies');
+        const newGiphy = new Giphy({ title, description, url });
+        await newGiphy.save();
+        console.log(newGiphy);
+        res.redirect('giphies/');
     }
 });
 
@@ -28,12 +33,8 @@ function validateGiphy(data) {
     if (!url) {
         errors.push({message: 'You must provide a url'});
     }
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ //port
-            '(\\?[;&amp;a-z\\d%_.~+=-]*)?'+ // query string
-            '(\\#[-a-z\\d_]*)?$','i');
+    var pattern =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
     if (!pattern.test(url)) {
         errors.push({message: 'Url is not valid'});
     }
