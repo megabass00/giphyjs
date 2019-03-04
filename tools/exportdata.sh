be32b5a7452a79f70ec40d19e6475bb5b2d4ebdd
Luis Izquierdo <luisma@jig.es>
2019-03-04 15:48:31 +0100

    Tools updated

--- tools/exportdata.sh
+++ tools/exportdata.sh
@@ -4,5 +4,5 @@ WORKING_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo 'Working path: '$WORKING_PATH;
 cd $WORKING_PATH;
 
-mongoexport --db giphy-app --collection giphies --out $WORKING_PATH"/data/giphies2.json"
-mongoexport --db giphy-app --collection users --out $WORKING_PATH"/data/users.json"
+mongoexport --db giphy-app --collection giphies --out $WORKING_PATH"/data/giphies.json" --jsonArray
+mongoexport --db giphy-app --collection users --out $WORKING_PATH"/data/users.json" --jsonArray
--- tools/filldata.sh
+++ tools/filldata.sh
@@ -6,6 +6,7 @@ cd $WORKING_PATH;
 
 echo "Recovering json files..."
 for file in $WORKING_PATH"/data"/*; do
-  echo "Importing $file..."
-  mongoimport --db giphy-app --collection giphies --authenticationDatabase admin --drop --file $file --jsonArray
+  filename=$(basename "$file" ".json")
+  echo "Importing collection: $filename"
+  mongoimport --db giphy-app --collection $filename --authenticationDatabase admin --drop --file $file --jsonArray
 done
