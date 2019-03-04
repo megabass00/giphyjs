1538eb7ca7bc43ef8f457c79097914b8d39edd22
Luis Izquierdo <luisma@jig.es>
2019-03-04 13:24:59 +0100

    Scripts updated

--- tools/exportdata.sh
+++ tools/exportdata.sh
@@ -5,3 +5,4 @@ echo 'Working path: '$WORKING_PATH;
 cd $WORKING_PATH;
 
 mongoexport --db giphy-app --collection giphies --out $WORKING_PATH"/data/giphies2.json"
+mongoexport --db giphy-app --collection users --out $WORKING_PATH"/data/users.json"
