6b0cf78d68cdb9dd7cf296f86eab102a9bc669a7
Luis Izquierdo <luisma@jig.es>
2019-03-05 16:35:53 +0100

    Signup api call implemented

--- src/routes/api.js
+++ src/routes/api.js
@@ -8,8 +8,8 @@ const { hasToken, getError } = require('../helpers/auth');
  * USERS
  */
 router.post('/api/signin', (req, res) => {
-    if (!req.body.email) { res.status(403).json(getError('Email is needed')); }
-    if (!req.body.password) { res.status(403).json(getError('Password is needed')); }
+    if (!req.body.email) { res.status(403).json(getError('Email is needed')); return; }
+    if (!req.body.password) { res.status(403).json(getError('Password is needed')); return; }
     const { email, password } = req.body;
     User.findOne({email: email}, async (err, user) => {
         if (err) { res.status(500).send(err); }
@@ -30,8 +30,33 @@ router.post('/api/signin', (req, res) => {
     });
 });
 
-router.post('/api/signup', (req, res) => {
-    
+router.post('/api/signup', async (req, res) => {
+    const { mastername, email, password, confirm_password } = req.body;
+    const errors = [];
+    if (password != confirm_password) {
+        errors.push({message: 'Password do not match'});
+    }
+    if (password.length < 4) {
+        errors.push({message: 'Password must be least 4 characters'});
+    }
+    if (errors.length > 0) {
+        res.status(403).json(getError('Missing data', errors));
+    }else{
+        const emailUser = await User.findOne({ email: email });
+        if (emailUser) {
+            res.status(403).json(getError('The email is already in use'));
+        }else{
+            const newUser = new User({ mastername, email, password });
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            const token = jwt.sign({newUser}, req.app.get('secret'));
+            res.status(200).json({
+                success: true,
+                user: newUser,
+                token
+            });
+        }
+    }
 });
 
 
@@ -41,8 +66,11 @@ router.post('/api/signup', (req, res) => {
  */
 router.get('/api/giphies/', hasToken, async (req, res) => {
     const giphies = await Giphy.find();
-    console.log(giphies.length);
-    res.json(giphies);
+    // console.log(`Return ${giphies.length} giphies`);
+    res.status(200).json({
+        success: true,
+        giphies
+    });
 });
 
 
